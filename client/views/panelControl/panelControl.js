f241ab7077ed48e891d1632b44cc27a8aa8f01a0
falcondamian <manuelfalcondamian@gmail.com>
2015-10-05 20:57:00 +0200

    Panel de control con consulta de fecha. Modificaciones de titulos

--- client/views/panelControl/panelControl.js
+++ client/views/panelControl/panelControl.js
@@ -1,3 +1,28 @@
+var repFechSubs;
+
+Template.panelControl.onCreated(function () {
+
+    var today = new Date();
+    today.setHours(0,0,0,0);
+
+    repFechSubs = Meteor.subscribe('repartosFecha', today);
+
+    Tracker.autorun(function () {
+        if (repFechSubs.ready()) {
+            var repartosAux = repartos.find().fetch();     
+            
+            console.log('Repartos cargados ' + repartosAux.length);
+
+            var idsRepartos = repartosAux.map(function(p) { return p._id });        
+            if (repartosAux && repartosAux.length > 0) {
+                Meteor.subscribe('pedidosOfRepartos', idsRepartos);
+            }
+        }
+    });    
+
+    Meteor.subscribe('clientes')
+});
+
 Template.panelControl.helpers({
 
     repartos: function() {
@@ -42,7 +67,10 @@ Template.panelControl.rendered = function() {
 
     $('#datetimepicker').datetimepicker({format:'DD/MM/YYYY'})
         .on('dp.change', function (ev) {
-            alert(ev.date);
-            console.log('Cambiar subscripcion con esta fecha nueva');  
+            if(repFechSubs) {
+               repFechSubs.stop(); 
+               console.log('Stop subscripcion');
+            }
+            repFechSubs = Meteor.subscribe('repartosFecha', ev.date.toDate()); 
     });
 }
